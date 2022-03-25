90b1f1b81a4e24a76488697fb78ed0d1ca829a6a
ileksei <dev.zhuravlev@gmail.com>
2022-03-26 01:29:50 +0300

    initial commit

---
+++ src/main/java/com/example/dynatracedemo/DynatraceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynatracedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynatraceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynatraceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynatracedemo/DynatraceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynatracedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynatraceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
